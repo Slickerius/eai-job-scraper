4a15d86a8dc5284594a0a7cb45a4481965f57c62
Faris Haidar Zuhdi <faris.haidar@ui.ac.id>
2023-05-10 17:30:25 +0700

    add scraper for kalibrr

---
+++ scraper/src/types/configs.ts
@@ -0,0 +1,11 @@
+type Config = {
+  urls: {
+    karir: string[];
+    jobstreet: string[],
+    kalibrr: string[],
+    linkedin: string[],
+  },
+  userAgent: string;
+};
+
+export default Config;
