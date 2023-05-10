7ab828ebbbe7b0464ddbddaff6503468fbd95386
Faris Haidar Zuhdi <faris.haidar@ui.ac.id>
2023-05-10 17:46:56 +0700

    add duplicate url checking

--- scraper-service/src/index.ts
+++ scraper-service/src/index.ts
@@ -18,20 +18,21 @@ const init = (async() => {
   await mongoose.connect(process.env.MONGO_URI).catch((err) => {
     console.error(err);
   });
+  
+  const config: Config = JSON.parse(fs.readFileSync(`./config.json`).toString());
 
   browser = await puppeteer.launch({ headless: false,
             executablePath: process.env.CHROME_BIN || undefined,
             args: [`--no-sandbox`, `--disable-gpu`, `--disable-dev-shm-usage`, 
-            `--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36`,
+            `--user-agent=${config.userAgent}`,
             `--user-data-dir=/tmp/user_data/`,
             `--start-maximized`] });
   
-  const config: Config = JSON.parse(fs.readFileSync(`./config.json`).toString());
   scrape(browser, config);
 });
 
-const scrape = (async(browser: any, config: Config) => {
+const scrape = async(browser: any, config: Config) => {
   scrapeKalibrr(browser, config);
-});
+};
 
 init();
